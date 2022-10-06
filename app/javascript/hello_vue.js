6f2e2f0507c4d06dd194adb9fb707c113f98f185
kkchart9 <koudai200109@gmail.com>
2022-10-06 16:00:06 +0900

    deploy環境確認

--- app/javascript/hello_vue.js
+++ app/javascript/hello_vue.js
@@ -1,9 +1,13 @@
 import { createApp } from 'vue'
 import App from './hello.vue'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+import 'ant-design-vue/dist/antd.variable.css'
+
 
 document.addEventListener('DOMContentLoaded', () => {
   const selector = '#js-hello-vue';
   if(document.querySelector(selector)){
-    createApp(App).mount(selector);
+    createApp(App).use(Antd).mount(selector);
   }
 })
